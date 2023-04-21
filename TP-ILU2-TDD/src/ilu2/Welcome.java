2dd0307662768fbf518f1e64c0dbdb7da93e07fb
OulKra <louis.gabernet@univ-tlse3.fr>
2023-04-21 14:51:22 +0200

    EX_2

--- TP-ILU2-TDD/src/ilu2/Welcome.java
+++ TP-ILU2-TDD/src/ilu2/Welcome.java
@@ -5,7 +5,8 @@ public class Welcome {
 
 	public static String welcome(String input)
 	{
+		if(input == null || input == "" || input.trim().equals(""))
+			return "Hello, my friend";
 		return "Hello, " + input.substring(0, 1).toUpperCase() + input.substring(1, input.length());		
 	}
-	
 }
--- TP-ILU2-TDD/src/ilu2/tests/WelcomeTest.java
+++ TP-ILU2-TDD/src/ilu2/tests/WelcomeTest.java
@@ -9,8 +9,16 @@ import ilu2.Welcome;
 public class WelcomeTest {
 
 	@Test
-	public void test()
+	public void testEX_1()
 	{
 		assertEquals(Welcome.welcome("bob"), "Hello, Bob");
+	}
+	
+	@Test
+	public void testEX_2()
+	{
+		assertEquals(Welcome.welcome(""), "Hello, my friend");
+		assertEquals(Welcome.welcome("    "), "Hello, my friend");
+		assertEquals(Welcome.welcome(null), "Hello, my friend");
 	}
 }
