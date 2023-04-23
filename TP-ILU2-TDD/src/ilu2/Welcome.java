d841d37636c1089b6e41a2c09653e7fbcd7fd4b3
oulkra <louis.gabernet@univ-tlse3.fr>
2023-04-23 17:06:01 +0200

    EX_8

--- TP-ILU2-TDD/src/ilu2/Welcome.java
+++ TP-ILU2-TDD/src/ilu2/Welcome.java
@@ -1,71 +1,110 @@
 package ilu2;
 
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Arrays;
 import java.util.Iterator;
+
 
 public class Welcome {
 
+	
+	static int existe(SimpleEntry<String, Integer>[] tableau, String val) 
+	{
+		int i = 0;
+		while(tableau[i] != null)
+		 {
+			 if(tableau[i].getKey().equals(val))
+				 return i;
+			 i++;
+		 }
+		 return -1;
+	}
+	
 	public static String welcome(String input)
 	{
+
 		if(input != null)
 		{
 			input = input.replaceAll("\\s", "");
 		}
-		try {
-			String[] name_list = input.split(",");
 
+		try 
+		{
+			String[] name_list = input.split(",");
+			
 			if(name_list.length > 1)
-			{	
-				String[] noms = new String[10];
-				String[] nomsCris = new String[10];
-				
+			{		
+
+				SimpleEntry<String, Integer>[] noms = new SimpleEntry[10];
+		        SimpleEntry<String, Integer>[] nomsCris = new SimpleEntry[10];
+ 				
 				int tailleNoms = 0;
 				int tailleNomsCris = 0;
 
 				String name = "";
 				String resCri = "HELLO";
+				System.out.println(Arrays.toString(name_list));
+
 				for (int i = 0; i < name_list.length; i++) 
 				{
+					int position;
+					System.out.println(name_list[i]);
 					if(name_list[i].equals(name_list[i].toUpperCase()))
 					{
-						nomsCris[tailleNomsCris] = name_list[i];
-						tailleNomsCris++;
+						if((position = existe(nomsCris, name_list[i])) != -1)
+							nomsCris[position].setValue(nomsCris[position].getValue() + 1);
+						else
+						{
+							nomsCris[tailleNomsCris] = new SimpleEntry<>(name_list[i], 1);
+							tailleNomsCris++;
+						}
+
 					}
 					else
 					{
-						noms[tailleNoms]= name_list[i].substring(0, 1).toUpperCase() + name_list[i].substring(1, name_list[i].length());
-						tailleNoms++;
+						System.out.println("POSITION : " + existe(nomsCris, name_list[i]));
+
+						if((position = existe(noms, name_list[i])) != -1)
+							noms[position].setValue(noms[position].getValue() + 1);
+						else
+						{
+							noms[tailleNoms] = new SimpleEntry<>(name_list[i].substring(0, 1).toUpperCase() + name_list[i].substring(1, name_list[i].length()), 1);
+							tailleNoms++;
+						}
+
 					}
 				}
 				
 				for (int i = 0; i < tailleNoms-1; i++) {
-					name += ", " + noms[i].substring(0, 1).toUpperCase() + noms[i].substring(1, noms[i].length());
+					name += ", " + noms[i].getKey().substring(0, 1).toUpperCase() + noms[i].getKey().substring(1, noms[i].getKey().length());
 				}
 				
 				for (int i = 0; i < tailleNomsCris-1; i++) {
-					resCri += ", "+nomsCris[i];
+					resCri += ", "+nomsCris[i].getKey();
 				}
 				
 				
 				if(tailleNomsCris != 0)
 				{
 					if(tailleNomsCris > 1)
-						resCri += " AND "+nomsCris[tailleNomsCris-1] + " !";
+						resCri += " AND "+nomsCris[tailleNomsCris-1].getKey() + " !";
 					else
-						resCri += ", "+nomsCris[tailleNomsCris-1] + " !";
+						resCri += ", "+nomsCris[tailleNomsCris-1].getKey() + " !";
 				}
 				
 				if(tailleNoms != 0)
 				{
 					if(tailleNoms > 1)
-						name += " and " + noms[tailleNoms-1].substring(0, 1).toUpperCase() + noms[tailleNoms-1].substring(1, noms[tailleNoms-1].length()) + ".";
+						name += " and " + noms[tailleNoms-1].getKey().substring(0, 1).toUpperCase() + noms[tailleNoms-1].getKey().substring(1, noms[tailleNoms-1].getKey().length()) + ".";
 					else
-						name += ", " + noms[tailleNoms-1].substring(0, 1).toUpperCase() + noms[tailleNoms-1].substring(1, noms[tailleNoms-1].length()) + ".";
+						name += ", " + noms[tailleNoms-1].getKey().substring(0, 1).toUpperCase() + noms[tailleNoms-1].getKey().substring(1, noms[tailleNoms-1].getKey().length()) + ".";
 				}
 				else
-				{
 					name += ".";
-				}
 				
+				System.out.println("name " + name);
+				System.out.println("resCri " + resCri);
+
 				if(resCri.equals("HELLO"))
 					return "Hello" + name;
 				return "Hello" + name + " AND " + resCri;
